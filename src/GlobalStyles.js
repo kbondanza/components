59538756fa18503baa29a3a3702d449fb93c1e21
Matt Hamlin <hamlim@outlook.com>
2019-07-04 18:25:18 -0400

    Add GlobalStyles reset component to the package

---
+++ src/GlobalStyles.js
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+import { jsx, css, Global } from '@emotion/core'
+
+export function GlobalStyles() {
+  return (
+    <Global
+      styles={theme => css`
+        html {
+          box-sizing: border-box;
+          font-size: ${theme.fontSizes[1]}px;
+        }
+        *,
+        *::before,
+        *::after {
+          box-sizing: inherit;
+        }
+        * {
+          margin: 0;
+          padding: 0;
+        }
+      `}
+    />
+  )
+}
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ export * from './Headings.js'
 export * from './Button.js'
 export * from './ThemeProvider.js'
 export * from './Input.js'
+export * from './GlobalStyles.js'
 
 export { default as Theme } from './Theme.js'
